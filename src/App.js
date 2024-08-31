1f7a4a94bc08e5cb54e170311578d18a77554f2d
Mihir Patel <mihirbhalani18@gmail.com>
2024-08-31 18:17:24 +0530

    Add some JS and CSS files for the project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
